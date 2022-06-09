901e262a614dbce041909f98017103df858170c9
Cengiz Şen <58827136+CengizSen@users.noreply.github.com>
2022-06-09 20:56:21 +0300

    İnheritance2

---
+++ inheritance2/src/inheritance2/Main.java
@@ -0,0 +1,15 @@
+package inheritance2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Logger logger=new EmailLogger();
+   
+    CustomerManager customerManager=new CustomerManager();
+    customerManager.Add(logger);
+	
+	
+	}
+
+}
